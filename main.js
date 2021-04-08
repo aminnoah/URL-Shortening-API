0f2230fe727722bb2fad92c25237261569e727c1
Noah <aminnnoah19@gmail.com>
2021-04-08 17:30:51 -0500

    css and accessibility edits

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const urlInput = document.getElementById('url-input');
 
 const originalLink = document.querySelector('.old-url');
 const shortenContent = document.querySelector('.shorten-content');
+const copyURL = document.querySelector('.btn-copy')
+
+// btnShort.addEventListener('click', shortLink);
+// copyURL.addEventListener('click', copyLink)
+
 
 // If display is mobile size, use menu bars for nav
 function menuMobile() {
@@ -16,19 +21,41 @@ function menuMobile() {
   });
 }
 
+// function copyLink(e) {
+//   console.log(e.target)
+//   // Check to only fire this action if we're clicking on the copy button
+//   if(e.target.classList.contains('btn-copy')) {
+//     console.log(e.target.value)
+//       // Select the input text field
+//       e.target.parentElement.children[1].select();
+//       e.target.parentElement.children[1].setSelectionRange(0, 99999); //For mobile devices
+  
+//       // Copy the text inside the text field 
+//       document.execCommand("copy");
+  
+//       // Change the appearance of the button to indicate successfull copying
+//       e.target.innerHTML = 'Copied!';
+//       e.target.style.backgroundColor = 'hsl(260, 8%, 14%)';
+//   }
+// }
+
 function shortLink() {
   btnShort.addEventListener('click', function (e) {
-    // if no value is placed return initial error
+    // if no value is placed return error
     if (urlInput.value == '') {
       error.style.visibility = 'initial';
     } else if (urlInput.value !== '') {
       // if value, hide error, send get request to url shortner
       error.style.visibility = 'hidden';
+      btnShort.innerHTML = 'Shortening...';
+      btnShort.disabled = true;
       const url = `https://api.shrtco.de/v2/shorten?url=${urlInput.value}`;
       fetch(url)
         .then((response) => response.json())
         .then((link) => {
           // create new element for url list
+          btnShort.innerHTML = 'Shorten It!';
+          btnShort.disabled = false;
           const urls = document.createElement('div');
           urls.classList.add('urls');
           shortenContent.appendChild(urls);
